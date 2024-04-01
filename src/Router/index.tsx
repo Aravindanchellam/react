fd9cb35a8d0ab3eda54a767c58dea1a57ca3ee0b
aravindan <aravindan1994c@gmail.com>
2024-04-01 22:04:53 +0530

    initial commit

---
+++ src/Router/index.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "../Screen/Home";
+import About from "../Screen/About";
+import Contact from "../Screen/Contact";
+
+function MainRouter() {
+    return(
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+        </Routes>
+    )
+}
+
+export default MainRouter;
---
+++ src/Screen/Home/index.tsx
@@ -0,0 +1,10 @@
+import "./Home.css";
+function Home() {
+    return(
+        <main className="main-container">
+            <div>Home</div>
+        </main>
+    )
+}
+
+export default Home;
